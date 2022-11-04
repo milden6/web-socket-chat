9e7ffd26efedcea9eb52b53c077ece883b703c69
mileev.daniil2 <mileev.daniil2@wb.ru>
2022-11-04 16:00:52 +0300

    Change UI

--- main.go
+++ main.go
@@ -36,9 +36,9 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	var addr = flag.String("addr", ":8080", "The addr of the app")
 	flag.Parse()
-	gomniauth.SetSecurityKey("uCuh3tMWIlMoikB5C75xdj7XjxiLBrJxCyFuxrTuPOE")
+	gomniauth.SetSecurityKey("SECRET KEY")
 	gomniauth.WithProviders(
-		google.New("853444431515-kr332t0q8d0mcn0pebbmvo9p1vqa4rdh.apps.googleusercontent.com", "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", "http://localhost:8080/auth/callback/google"),
+		google.New("KEY", "SECRET", "CALLBACK"),
 	)
 	r := chat.NewRoom()
 
@@ -46,6 +46,16 @@ func main() {
 	http.Handle("/chat", chat.MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.HandleFunc("/auth/", chat.LoginHandler)
+	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		http.SetCookie(w, &http.Cookie{
+			Name:   "auth",
+			Value:  "",
+			Path:   "/",
+			MaxAge: -1,
+		})
+		w.Header().Set("Location", "/chat")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	})
 	http.Handle("/room", r)
 
 	go r.Run()
